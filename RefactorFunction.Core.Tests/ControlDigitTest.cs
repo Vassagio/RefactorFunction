80507563b48c6a994d8c508a77e62c05a5f4dc91
William C. Chronowski <wchronowski@pekininsurance.com>
2017-07-19 12:34:21 -0500

    Initial

---
+++ RefactorFunction.Core.Tests/ControlDigitTest.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace RefactorFunction.Core.Tests {
+    public class ControlDigitTest {
+        [Fact]
+        public void Initialize() {
+            var controlDigit = new ControlDigit();
+
+            Assert.NotNull(controlDigit);
+        }
+
+        [Theory]
+        [InlineData(123456, 3)]
+        [InlineData(0, 0)]
+        [InlineData(9876543210, 1)]
+        public void Calculate_ReturnsModulo(long number, int expected) {
+            var controlDigit = new ControlDigit();
+
+            var result = controlDigit.Calculate(number);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ RefactorFunction.Core/ControlDigit.cs
@@ -0,0 +1,21 @@
+﻿namespace RefactorFunction.Core {
+    public class ControlDigit {
+        public int Calculate(long number) {
+            var sum = 0;
+            var isOddPosition = true;
+
+            while (number > 0) {
+                var digit = (int)(number % 10);
+                if (isOddPosition)
+                    sum += 3 * digit;
+                else
+                    sum += digit;
+                number /= 10;
+                isOddPosition = !isOddPosition;
+            }
+
+            var modulo = sum % 7;
+            return modulo;
+        }
+    }
+}
